0a8506211ce4ba921f84a7eb931a274b30465ca5
Pierpaolo Gallo <43746402+pierpaolo17@users.noreply.github.com>
2026-05-08 10:04:36 +0200

    fix: restore correct version number for v1.0.14

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotrack';
 
-$plugin->version = 2026050507;
+$plugin->version = 20260505700;
 $plugin->requires = 2025041400; // Moodle 5.0.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '1.0.14';
