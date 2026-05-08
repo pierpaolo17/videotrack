d1e9c8097b6dcf0f2a33057aec0f15ff3382dcdd
Pierpaolo Gallo <43746402+pierpaolo17@users.noreply.github.com>
2026-05-08 10:03:05 +0200

    fix: resolve version.php conflict, align main to v1.0.14

--- version.php
+++ version.php
@@ -3,9 +3,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotrack';
-<<<<<<< HEAD
-plugin->version = 2026050507;
+
+$plugin->version = 2026050507;
 $plugin->requires = 2025041400; // Moodle 5.0.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '1.0.13';
->>>>>>> release/v1.0.14
+$plugin->release = '1.0.14';
+
