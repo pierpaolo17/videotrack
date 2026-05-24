6c9fea9394cc8ae7e5f5762525e60317adc8d4d8
Pierpaolo Gallo <43746402+pierpaolo17@users.noreply.github.com>
2026-05-24 22:31:40 +0200

    Release 1.3.18

--- version.php
+++ version.php
@@ -4,9 +4,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotrack';
 
-$plugin->version = 2026072520;
+$plugin->version = 2026072521;
 $plugin->requires = 2025041400; // Moodle 5.0.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '1.3.17-dev';
+$plugin->release = '1.3.18-dev';
 $plugin->supported = [500, 502];
 $plugin->dependencies = [];
